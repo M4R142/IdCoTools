9cf0d5ca65a3c15c76ca7df74c659faf00e69de7
mpt1002 <57134968+mpt1002@users.noreply.github.com>
2022-04-26 18:46:51 +0200

    Interfaz - IDatabase - Añadido definición de funciones. #3

--- IdCoTools/Models/Database/IDatabase.cs
+++ IdCoTools/Models/Database/IDatabase.cs
@@ -1,7 +1,32 @@
 ﻿
 namespace IdCoTools.Models.Database
 {
-    interface IDatabase
+    public interface IDatabase
     {
+        /// <summary>
+        /// Crear una tabla sino existe en la BD
+        /// </summary>
+        void CreateTable();
+        /// <summary>
+        /// Eliminar una tabla de la BD, siempre y cuando esta exista.
+        /// </summary>
+        void DropTable();
+        /// <summary>
+        /// Contar el numero de elementos que tiene una tabla en una BD.
+        /// </summary>
+        /// <returns></returns>
+        int Count();
+        /// <summary>
+        /// Guardar una persona en la base de datos.
+        /// </summary>
+        /// <param name="person"></param>
+        /// <returns></returns>
+        int SavePerson(Person.Person person);
+        /// <summary>
+        /// Eliminar una persona de la base de datos.
+        /// </summary>
+        /// <param name="person"></param>
+        /// <returns></returns>
+        int RemovePerson(Person.Person person);
     }
 }
