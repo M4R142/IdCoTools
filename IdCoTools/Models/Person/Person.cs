4e9dab483a7bd2dc03788c1f856828b92a1329cf
mpt1002 <57134968+mpt1002@users.noreply.github.com>
2022-04-26 18:10:03 +0200

    Clase - Person - Añadida la estructura de la tabla. #2

--- IdCoTools/Models/Person/Person.cs
+++ IdCoTools/Models/Person/Person.cs
@@ -1,7 +1,23 @@
-﻿
+﻿using SQLite;
+
 namespace IdCoTools.Models.Person
 {
-    class Person
+    /// <summary>
+    /// Estructura de datos para asociar un ID (único), personId, faceId, name, lastname y photo.
+    /// </summary>
+    public class Person
     {
+        [PrimaryKey, AutoIncrement, Column("ID")]
+        public int ID { get; set; }
+        [Column("PersonId")]
+        public string PersonId { get; set; }
+        [Column("FaceId")]
+        public string FaceId { get; set; }
+        [Column("Name")]
+        public string Name { get; set; }
+        [Column("LastName")]
+        public string LastName { get; set; }
+        [Column("Photo")]
+        public byte[] Photo { get; set; }
     }
 }
