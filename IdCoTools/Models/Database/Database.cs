c967dafe8777b721b78fc9548c7ec6919b7b73e7
mpt1002 <57134968+mpt1002@users.noreply.github.com>
2022-04-27 17:27:45 +0200

    Clase - Database - Implementacion de la interfaz IDatabase y las correspondientes funcionalidades de los métodos. #3

--- IdCoTools/Models/Database/Database.cs
+++ IdCoTools/Models/Database/Database.cs
@@ -1,7 +1,59 @@
-﻿
+﻿using SQLite;
+using System.Threading.Tasks;
+
 namespace IdCoTools.Models.Database
 {
-    class Database
+    public class Database : IDatabase
     {
+        SQLiteAsyncConnection database;
+        /// <summary>
+        /// Inicializar la conexión de la base de datos.
+        /// </summary>
+        /// <param name="name">Nombre del path + base de datos</param>
+        public Database(string name)
+        {
+            database = new SQLiteAsyncConnection(name);
+        }
+        public int Count()
+        {
+            Task<int> count = database.Table<Person.Person>().CountAsync();
+            return count.Result;
+        }
+        /// <summary>
+        /// Crear la tabla Person en la base de datos.
+        /// </summary>
+        public void CreateTable()
+        {
+            database.CreateTableAsync<Person.Person>().Wait();
+        }
+        /// <summary>
+        /// Eliminar la tabla Person en la base de datos.
+        /// </summary>
+        public void DropTable()
+        {
+            database.DropTableAsync<Person.Person>().Wait();
+        }
+        /// <summary>
+        /// Eliminar un objeto Person de la base de datos.
+        /// </summary>
+        /// <param name="person">Person a eliminar de la BD</param>
+        /// <returns>Numero de filas de la BD eliminadas</returns>
+        public int RemovePerson(Person.Person person)
+        {
+            Task<int> num = database.DeleteAsync(person);
+            return num.Result;
+        }
+        /// <summary>
+        /// Guardar un objeto Person en la base de datos.
+        /// </summary>
+        /// <param name="person">Guardar un objeto Person en la BD</param>
+        /// <returns>numero de filas añadidas en la BD</returns>
+        public int SavePerson(Person.Person person)
+        {
+            person.Name = person.Name.ToUpper();
+            person.LastName = person.LastName.ToUpper();
+            Task<int> num = database.InsertAsync(person);
+            return num.Result;
+        }
     }
 }
