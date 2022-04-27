34718711709474bd529f03ea876dc32a4d070f50
mpt1002 <57134968+mpt1002@users.noreply.github.com>
2022-04-27 17:51:41 +0200

    Nuevo Windows Forms - IdCoToolConsole - Añadido nuevo formulario de tipo Windows Forms donde se va ha implementar la interfaz de usuario. #4

--- IdCoTools/IdCoToolConsole.cs
+++ IdCoTools/IdCoToolConsole.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace IdCoTools
 {
-    public partial class Form1 : Form
+    public partial class IdCoToolConsole : Form
     {
-        public Form1()
+        public IdCoToolConsole()
         {
             InitializeComponent();
         }
